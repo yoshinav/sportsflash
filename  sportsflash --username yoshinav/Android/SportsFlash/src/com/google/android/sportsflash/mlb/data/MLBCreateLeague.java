6641be672755bf677b4ae9a9bb45dbf9086a22f6
yoshinav <yoshinav@a3f552c8-f74e-0410-9b22-a31d960dc3f1>
2008-07-21 21:52:27 +0000

    refactored

---
+++  sportsflash --username yoshinav/Android/SportsFlash/src/com/google/android/sportsflash/mlb/data/MLBCreateLeague.java
@@ -0,0 +1,93 @@
+package com.google.android.sportsflash.mlb.data;
+
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.util.Log;
+
+import com.google.android.sportsflash.Configuration;
+import com.google.android.sportsflash.mlb.teammanagement.Constants;
+
+/**
+ * MLBCreateLeague:  Create MLB Fantasy League Team
+ * 
+ * @author Navdeep Alam
+ * @version CS 893 Summer 2008 Version 1.0
+ * 
+ */
+public class MLBCreateLeague {
+
+    private static final String CLASSTAG = MLBCreateLeague.class.getSimpleName();
+
+    private String query;
+    private boolean debug;
+    
+    public MLBCreateLeague(boolean debug) {
+
+        this.debug = debug;
+
+        // build query
+        this.query = Configuration.urlCreateMLBLeague;
+        Log.v(Constants.LOGTAG, " " + CLASSTAG + " query - " + query);
+    }
+    
+    public MLBCreateLeague() {
+        this(false);
+    }
+    
+    public int CreateLeague()
+    {
+    	return CreateLeague("Navi League", "This is a test League for MLB");
+    }
+    
+    public int CreateLeague(String name, String description)
+    {
+        Log.v(Constants.LOGTAG, " " + CLASSTAG + " getReviews");
+        long start = System.currentTimeMillis();
+
+
+        if (!debug) {
+            try {
+            	//setup the url
+                URL feedUrl = new URL(this.query + "name=" + name + "&description=" + description);
+                // TODO - huge delay here on build call, takes 15-20 seconds 
+                // (takes < second for same class outside Android)
+                
+                // create the factory
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                // create a parser
+                SAXParser parser = factory.newSAXParser();
+
+                // create the reader (scanner)
+                XMLReader xmlreader = parser.getXMLReader();
+                // instantiate our handler
+                MLBLeagueHandler theMLBLeagueHandler = new MLBLeagueHandler();
+                // assign our handler
+                xmlreader.setContentHandler(theMLBLeagueHandler);
+                // get our data through the url class
+                InputSource is = new InputSource(feedUrl.openStream());
+                // perform the synchronous parse           
+                xmlreader.parse(is);
+                // get the results - should be a fully populated RSSFeed instance, 
+     		   // or null on error
+                return theMLBLeagueHandler.getMLBID();
+  
+                
+            } catch (Exception e) {
+                Log.e(Constants.LOGTAG, " " + CLASSTAG + " getReviews ERROR", e);
+            }
+        } else {
+            Log.v(Constants.LOGTAG, " " + CLASSTAG + " devMode true - returning MOCK reviews");
+            return 0;
+        }
+
+        long duration = (System.currentTimeMillis() - start) / 1000;
+        Log.v(Constants.LOGTAG, " " + CLASSTAG + " call duration - " + duration);
+        return 0;
+    }    
+}
