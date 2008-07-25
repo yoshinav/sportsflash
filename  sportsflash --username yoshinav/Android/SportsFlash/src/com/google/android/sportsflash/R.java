bc10cdbed0f1e285dda777be3a98722ad18b6507
yoshinav <yoshinav@a3f552c8-f74e-0410-9b22-a31d960dc3f1>
2008-07-25 13:32:15 +0000

    Updated UI

---  sportsflash --username yoshinav/Android/SportsFlash/src/com/google/android/sportsflash/R.java
+++  sportsflash --username yoshinav/Android/SportsFlash/src/com/google/android/sportsflash/R.java
@@ -11,10 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int solid_blue=0x7f020002;
-        public static final int solid_green=0x7f020003;
-        public static final int solid_red=0x7f020001;
+        public static final int baseballtexture2=0x7f020000;
+        public static final int bballtexture2=0x7f020001;
+        public static final int footballtexture2=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int solid_blue=0x7f020006;
+        public static final int solid_green=0x7f020007;
+        public static final int solid_red=0x7f020005;
+        public static final int swing=0x7f020004;
     }
     public static final class id {
         public static final int empty=0x7f060000;
